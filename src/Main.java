6047c626ffe54036b83dbe1c1253d20b4efa5107
Imario Almeida <imario.almeida22@gmail.com>
2023-03-12 11:43:48 -0300

    Adição das classes Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        DateTimeFormatter formatadorBarra = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso java");
+        curso1.setCargaHoraria(10);
+
+        Mentoria mentoria1 = new Mentoria();
+
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Descrição mentoria java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(mentoria1);
+
     }
 }
